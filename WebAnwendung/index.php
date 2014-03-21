41cabc59dd64d80fd8ad3b8f3a8dbf3e540453a7
LzyPanda <LzyPanda@lepus.uberspace.de>
2014-03-21 11:26:44 +0100

    Webanwendung
    Pfeiltastenreaktionen hinzugefügt

--- WebAnwendung/index.php
+++ WebAnwendung/index.php
@@ -3,7 +3,17 @@
 <head>
 	<script type="text/javascript" src="jquery-2.1.0.min.js"></script>
 	<script type="text/javascript">
-		var button='';
+	var button='';
+	$(document).keydown(function(e){
+    if (e.keyCode == 37) { 				//code für linke pfeiltaste
+       	button='left';
+       	scriptAusfuehren();   
+    }	
+    else if(e.keyCode==39) {			//code für rechte pfeiltaste
+   		button='right';
+      	scriptAusfuehren();
+    }
+    });	
 		function scriptAusfuehren(){
 			$.ajax({ url: './Script.php',
      	    	data: {action: button},
