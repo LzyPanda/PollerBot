4bf7c9e5aa4168830fb8f000b59b0cd595149776
CroMarmot <spam.cromarmot@gmail.com>
2014-04-25 16:37:13 +0200

    web_control Flask methods

--- raspberry/web_control.py
+++ raspberry/web_control.py
@@ -1,12 +1,22 @@
+#!/usr/bin/env python
+
 __author__ = 'CroMarmot-OSX'
 #20140413
 #TODO: left-forward e.g. 25/25 or 50/50
 
 import RPi.GPIO as GPIO
 import signal
-import sys
+#import sys
+
+from flask import Flask, render_template
 
 from pizypwm import *
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+  return render_template('/var/www/index.html')
 
 def endProcess(signalnum = None, handler = None):
     first.stop()
@@ -34,38 +44,43 @@ GPIO.setup(21,GPIO.OUT)
 GPIO.setup(23,GPIO.OUT)
 GPIO.setup(24,GPIO.OUT)
 
+@app.route('/forward/')
 def forward():
     first.start(50)
     second.start(0)
     third.start(0)
     fourth.start(50)
-    print "Geradeaus vorwaerts"
+#    print "Geradeaus vorwaerts"
     return
 
+@app.route('/reverse/')
 def reverse():
     first.start(0)
     second.start(50)
     third.start(50)
     fourth.start(0)
-    print "Geradeaus rueckwaerts"
+#    print "Geradeaus rueckwaerts"
     return
 
+@app.route('/link/')
 def left():
     first.start(0)
     second.start(50)
     third.start(0)
     fourth.start(50)
-    print "Drehung nach Links"
+#    print "Drehung nach Links"
     return
 
+@app.route('/right/')
 def right():
     first.start(50)
     second.start(0)
     third.start(50)
     fourth.start(0)
-    print "Drehung nach rechts"
+#    print "Drehung nach rechts"
     return
 
+@app.route('/stop/')
 def stop():
     first.start(0)
     second.start(0)
@@ -74,15 +89,5 @@ def stop():
     print "Stop"
     return
 
-controlarg = sys.argv[0]
-
-if controlarg == 'forward':
-    forward()
-elif controlarg == 'reverse':
-    reverse()
-elif controlarg == 'left':
-    left()
-elif controlarg == 'right':
-    right()
-elif controlarg == 'stop':
-    stop()
+if __name__ == '__main__':
+  app.run()
